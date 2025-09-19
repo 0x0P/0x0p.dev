916bb8aa16180cbfc9f48369e3fa4cab3f8168c7
PXXCXNT <pxxcxnt@icloud.com>
2025-09-20 05:04:11 +0900

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import styles from './main.module.css';
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className={styles.main}>
+      <img src={'/me2.png'} alt='me' />
+      <h1>PERCENTZERO</h1>
+
     </main>
   );
 }
